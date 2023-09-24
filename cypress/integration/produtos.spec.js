c6b8d760cb7b99763c39137350cb32362d047a5d
Maria Tereza <mtereza.cl@gmail.com>
2023-09-24 13:42:27 -0300

    meu primeiro commit

---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade de produtos' , () =>{
+
+    beforeEach(() =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+        //first()
+        //.last()
+        //eq(3)
+        .contains('Abominable Hoodie')
+        .click()
+
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+
+        cy.get('[class="product-block grid"]')
+            .contains('Abominable Hoodie')
+            .click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should('contain' , quantidade + ' ×')
+
+    });
+
+
+})
